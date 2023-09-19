e461fdb2756043d9112ab61d977cc0adf884def8
BSY <45937750+ideawolf@users.noreply.github.com>
2023-09-19 17:49:03 +0900

    Home

--- client/src/App.js
+++ client/src/App.js
@@ -2,13 +2,15 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Add from "./pages/Add";
 import Update from "./pages/Update";
 import Members from "./pages/Members";
+import Home from "./pages/Home";
 
 function App() {
   return (
     <div className="app">
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<Members />} />
+          <Route path="/" element={<Home />} />
+          <Route path="/members" element={<Members />} />
           <Route path="/add" element={<Add />} />
           <Route path="/update/:id" element={<Update />} />
         </Routes>
---
+++ client/src/pages/Home.jsx
@@ -0,0 +1,61 @@
+import axios from "axios";
+import React, { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import backendUrl from '../config';
+
+const Add = () => {
+  const [member, setMember] = useState({
+    name: "",
+    student_number: "",
+    photo_url: "",
+  });
+  const [error, setError] = useState(false);
+
+  const navigate = useNavigate();
+
+  const handleChange = (e) => {
+    setMember((prev) => ({ ...prev, [e.target.name]: e.target.value }));
+  };
+
+  const handleClick = async (e) => {
+    e.preventDefault();
+    try {
+      await axios.post(`${backendUrl}/members`, member);
+      navigate("/"); 
+    } catch (err) {
+      console.log(err);
+      setError(true);
+    }
+  };
+
+  return (
+    <div className="form">
+      <h1>Add New Member</h1>
+      <div className="input_forms">
+        <input
+          type="text"
+          placeholder="Member name"
+          name="name"
+          onChange={handleChange}
+        />
+        <input
+          type="text"
+          placeholder="Student number"
+          name="student_number"
+          onChange={handleChange}
+        />
+        <input
+          type="text"
+          placeholder="Photo URL"
+          name="photo_url"
+          onChange={handleChange}
+        />
+        <button onClick={handleClick}>Add</button>
+        {error && "Something went wrong!"}
+        <Link to="/">See all members</Link>
+      </div>
+    </div>
+  );
+};
+
+export default Add;
