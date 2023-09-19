7f68b2ca5b91e2011049c645a22b7f4313caad24
BSY <45937750+ideawolf@users.noreply.github.com>
2023-09-19 17:51:30 +0900

    Update Home.jsx

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,69 +1,17 @@
-import React, { useEffect, useState } from "react";
-import axios from "axios";
+import React from "react";
 import { Link } from "react-router-dom";
-import backendUrl from '../config';
 
-const Members = () => {
-  const [members, setMembers] = useState([]);
-
-  useEffect(() => {
-    const fetchAllMembers = async () => {
-      try {
-        const res = await axios.get(`${backendUrl}/members`); 
-        setMembers(res.data);
-      } catch (err) {
-        console.log(err);
-      }
-    };
-    fetchAllMembers();
-  }, []);
-
-  const handleDelete = async (id) => {
-    try {
-      await axios.delete(`${backendUrl}/members/${id}`);
-      window.location.reload();
-    } catch (err) {
-      console.log(err);
-    }
-  };
-
+const Home = () => {
   return (
     <div>
-      <h1>Whyrony Members</h1>
-      <div className="members">
-        {members.map((member) => (
-          <div key={member.id} className="member">
-            <img src={member.photo_url} alt="" />
-            <h3>{member.name}</h3>
-            <p>ID: {member.student_number}</p>
-            <button className="delete" onClick={() => handleDelete(member.id)}>
-              Delete
-            </button>
-            <button className="update">
-              <Link
-                to={`/update/${member.id}`}
-                style={{ color: "inherit", textDecoration: "none" }}
-              >
-                Update
-              </Link>
-            </button>
-          </div>
-        ))}
-      </div>
-
+      <h1>Welcome to Whyrony</h1>
       <button className="addHome">
-        <Link to="/add" style={{ color: "inherit", textDecoration: "none" }}>
-          Add new member
+        <Link to="/members" style={{ color: "inherit", textDecoration: "none" }}>
+          View Members
         </Link>
       </button>
-
-      {/* <button className="addHome">
-        <Link to="/add" style={{ color: "inherit", textDecoration: "none" }}>
-          Add Random member
-        </Link>
-      </button> */}
     </div>
   );
 };
 
-export default Members;
+export default Home;
