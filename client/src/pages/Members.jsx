31585508e22d6f8472c1af951bf5f83e9f9ba600
BSY <45937750+ideawolf@users.noreply.github.com>
2023-09-19 14:54:44 +0900

    Update Members.jsx

--- client/src/pages/Members.jsx
+++ client/src/pages/Members.jsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
+import { Link } from "react-router-dom";
 
 const Members = () => {
   const [members, setMembers] = useState([]);
@@ -16,6 +17,15 @@ const Members = () => {
     fetchAllMembers();
   }, []);
 
+  const handleDelete = async (id) => {
+    try {
+      await axios.delete(`http://localhost:4000/members/${id}`);
+      window.location.reload();
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
   return (
     <div>
       <h1>Team Members</h1>
@@ -25,9 +35,29 @@ const Members = () => {
             <img src={member.photo_url} alt="" />
             <h2>{member.name}</h2>
             <p>Student Number: {member.student_number}</p>
+            <button className="delete" onClick={() => handleDelete(member.id)}>
+              Delete
+            </button>
+            <button className="update">
+              <Link
+                to={`/update/${member.id}`}
+                style={{ color: "inherit", textDecoration: "none" }}
+              >
+                Update
+              </Link>
+            </button>
           </div>
         ))}
       </div>
+
+      <button className="addHome">
+        <Link
+          to="/add"
+          style={{ color: "inherit", textDecoration: "none" }}
+        >
+          Add new member
+        </Link>
+      </button>
     </div>
   );
 };
