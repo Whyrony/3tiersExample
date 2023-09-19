79fe78fb372e89af02a1f23caba77a21f7d85e92
BSY <45937750+ideawolf@users.noreply.github.com>
2023-09-19 15:01:49 +0900

    북제거

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,5 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Add from "./pages/Add";
-import Books from "./pages/Books";
 import Update from "./pages/Update";
 import Members from "./pages/Members";
 
